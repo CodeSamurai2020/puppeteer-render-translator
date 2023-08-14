cb3a31932a2b135e4f089b16cb9cf680dd4e9bd2
CodeSamurai2020 <142196178+CodeSamurai2020@users.noreply.github.com>
2023-08-14 10:56:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
+const translate =  require('./translate')
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 4000;
-
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+app.get('/', (req, res) => {
+  res.send('Welcome to my custom translator api');
 });
+app.get('/translate/:sourceLang/:targetLang/:textToTranslate', translate.translateText)
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
